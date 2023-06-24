3f6718dc6d5136f36a410f187527353b7eea879c
ofay11 <itimurdautov@gmail.com>
2023-06-24 22:24:31 +0300

    Added prime number game

---
+++ src/games/prime.js
@@ -0,0 +1,35 @@
+import { randomInteger, gameBase } from '../index.js';
+
+const primeGame = () => {
+  const question =
+    'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+  const isPrime = (number) => {
+    if (number < 2) {
+      return 'no';
+    }
+
+    let divider = 2;
+
+    while (divider <= number / 2) {
+      if (number % divider === 0) {
+        return 'no';
+      }
+      divider += 1;
+    }
+
+    return 'yes';
+  };
+
+  // generation of tasks and solutions
+  const randomTask = () => {
+    const task = randomInteger(1, 100);
+    const solution = isPrime(task);
+
+    return [task, solution];
+  };
+
+  gameBase(question, randomTask);
+};
+
+export default primeGame;
