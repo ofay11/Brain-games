a1df3db4b350f9daa79c0f80ba624f1444fc0ee0
ofay11 <itimurdautov@gmail.com>
2023-06-26 20:56:51 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -22,9 +22,7 @@ export const gameBase = (question, randomTask) => {
     if (userAnswer === String(solution)) {
       console.log('Correct!');
     } else {
-      console.log(
-        `"${userAnswer}" is wrong answer ;(. Correct answer was "${solution}".\nLet's try again, ${userName}!`
-      );
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${solution}".\nLet's try again, ${userName}!`);
       return;
     }
   }
